960ea39c590d5895397a86a782cc2de795e2e321
Chris Weyl <cweyl@alumni.drew.edu>
2011-04-15 19:02:42 -0700

    Additional native Hash accessors
    grep_keys, grep_values
    map_keys, map_values
    sort_keys, sort_values

---
+++ lib/Moose/Meta/Method/Accessor/Native/Hash/grep_values.pm
@@ -0,0 +1,27 @@
+package Moose::Meta::Method::Accessor::Native::Hash::grep_values;
+
+# This is an extension to the "Hash" attribute native trait... Since 1.15,
+# each accessor to a native trait has its own metaclass, meaning that we can
+# "add" new ones simply by defining another method metaclass of the right
+# name.
+
+use Moose::Role 1.15;
+use namespace::autoclean;
+use common::sense;
+
+with
+    'Moose::Meta::Method::Accessor::Native::Array::grep' => {
+        -excludes => [qw{ _return_value }],
+        -alias    => { _return_value => '_array_return_value' },
+    },
+    'Moose::Meta::Method::Accessor::Native::Hash',
+    ;
+
+sub _return_value
+{
+    my ($self, $slot_access) = @_;
+
+    return $self->_array_return_value("[ values %{ $slot_access } ]");
+}
+
+1;
